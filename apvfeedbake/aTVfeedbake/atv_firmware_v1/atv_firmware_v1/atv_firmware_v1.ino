ad6050309ef96ca8f76f9497cb822e708c01fc3b
Shaurjya Banerjee <shaurjyab@gmail.com>
2018-03-04 20:31:33 -0800

    updated functions

--- apvfeedbake/aTVfeedbake/atv_firmware_v1/atv_firmware_v1/atv_firmware_v1.ino
+++ apvfeedbake/aTVfeedbake/atv_firmware_v1/atv_firmware_v1/atv_firmware_v1.ino
@@ -1,3 +1,6 @@
+//APVFeedBakE CRT Firmware V0.0.0
+//James Anderson and Shaurjya Banerjee 2018
+
 #include <TVout.h>
 #include <TVoutfonts/fontALL.h>
 #include "schematic.h"
@@ -5,10 +8,13 @@
 
 TVout TV;
 
-//--------------------------------------------------------------------------
+//VARIABLES-------------------------------------------------------------------------
 
-int enter_state = 0;
-int menu_state = 1;
+byte enter_state = 0;
+byte menu_state = 1;
+byte print_state = 0;
+
+byte osc_state = 0;
 
 const byte button1_pin = 13;
 const byte button2_pin = 12;
@@ -24,26 +30,34 @@ byte button1_state = 0;
 byte button2_state = 0;
 byte button3_state = 0;
 byte button4_state = 0;
+byte trig1_state = 0;
+byte trig2_state = 0;
 
 int pot1_val = 0;
 int pot2_val = 0;
 int pot3_val = 0;
 int pot4_val = 0;
 
-int t = 69;
+const byte relayh  = 0;
+const byte relayv  = 1;
+const byte relaym1 = 2;
+const byte relaym2 = 3;
 
-byte print_state = 0;
+const byte trig1_pin = 5;
+const byte trig2_pin = 6;
+
+int t = 69;
 
 const byte button_delay = 300;
 
 void setup() 
-{
-  
-  //Set pinmode for digital pins
+{ 
   pinMode(button1_pin, INPUT);
   pinMode(button2_pin, INPUT);
   pinMode(button3_pin, INPUT);
   pinMode(button4_pin, INPUT);
+  pinMode(trig1_pin, INPUT);
+  pinMode(trig2_pin, INPUT);
 
   for (int i = 0; i < 4; i++) {
     pinMode(i, OUTPUT);
@@ -63,14 +77,13 @@ void loop()
 
 //---------------------------------------------------------------------------------
 
-void fast_bytebeat()
+void bytebeat_le()
 {
   while(button1_state != HIGH)
   {
     pot1_val = analogRead(pot1_pin);
     pot2_val = analogRead(pot2_pin);
     pot3_val = analogRead(pot3_pin);
-    
     button1_state = digitalRead(button1_pin);
 
     byte shift_amt_0 = pot2_val >> 8;
@@ -87,39 +100,107 @@ void fast_bytebeat()
     t >> shift_amt_1;
     
     bbval = bbval >> post_bbcalc_shift;
-    
     bbval = bbval & 0b1111;
-    
     PORTD = bbval;
-
     
-    
-    //pot1_val*= 1.0;
     delayMicroseconds(5 + pot1_val);
-  }
-  
+  } 
 }
 
-void slow_bytebeat()
+void bytebeat_editor()
 {
   
 }
+
+void oscillopgraphics(byte state)
+{
+  if (state == HIGH)
+  {
+    digitalWrite(relayh, LOW);
+    digitalWrite(relayv, LOW);
+  }
+  else if (state == LOW)
+  {
+    digitalWrite(relayh, HIGH);
+    digitalWrite(relayv, HIGH);
+  } 
+}
+
+void cv_tv()
+{
+  if (trig1_state == LOW)
+  {
+    digitalWrite(relayh, LOW);
+    digitalWrite(relayv, LOW);
+  }
+
+  if (trig1_state == HIGH)
+  {
+    digitalWrite(relayh, HIGH);
+    digitalWrite(relayv, HIGH);
+  }
+
+  if (trig2_state == LOW)
+  {
+    digitalWrite(relaym1, LOW);
+    digitalWrite(relaym2, LOW);
+  }
+
+  if (trig2_state == HIGH)
+  {
+    digitalWrite(relaym1, HIGH);
+    digitalWrite(relaym2, HIGH);
+  }
+}
+
+
+//GFX------------------------------------------------------------------------------
 
 void gfx_handler()
 {
-  //MENU1 - FAST BYTEBEAT
+  //MENU1 - Bytebeat LE
   if (enter_state != 0 && menu_state == 1)
   {
     TV.clear_screen();
-    fast_bytebeat_gfx();
+    bytebeat_le_gfx();
   }
 
-  //MENU2 - SLOW BYTEBEAT
+  //MENU2 - BYTEBEAT EDITOR
   if (enter_state != 0 && menu_state == 2)
   {
     TV.clear_screen();
-    slow_bytebeat_gfx();
+    bytebeat_editor_gfx();
   }
+
+  //MENU3 - CV TV 
+  if (enter_state != 0 && menu_state == 3)
+  {
+    TV.clear_screen();
+    cv_tv_gfx();
+  }
+
+  //MENU4 - MAGNETS
+  if (enter_state != 0 && menu_state == 4)
+  {
+    TV.clear_screen();
+    
+  }
+
+  //MENU5 - OSCILLOGRAPHIX
+  if (enter_state != 0 && menu_state == 5)
+  {
+    TV.clear_screen();
+    oscillopgraphics(HIGH);
+  }
+
+  //MENU6 - DEBUG
+  if (enter_state != 0 && menu_state == 6)
+  {
+    TV.clear_screen();
+    oscillopgraphics(HIGH);
+  }
+
+  
 
   //If we are not currently in a mode, 
   //Go back to main mode choosing menu
@@ -131,32 +212,37 @@ void gfx_handler()
   }
 }
 
-void slow_bytebeat_gfx()
+void bytebeat_editor_gfx()
 {
   TV.select_font(font8x8);
-  TV.println("SLOW BYTEBEAT");
+  TV.println("BYTEBEAT EDITOR");
 
   if (button3_state == HIGH)
   {
-    slow_bytebeat();
+    bytebeat_editor();
   }
 }
 
-void fast_bytebeat_gfx()
+void bytebeat_le_gfx()
 {
   TV.select_font(font8x8);
-  TV.println("FAST BYTEBEAT");
+  TV.println("BYTEBEAT LE");
   TV.println("");
 
   TV.select_font(font6x8);
   TV.println("((t << 1) ^ ((t << 1) + (t >> 7) & t >> 12)) | t >> (4 - (1 ^ 7 & (t >> 15))) | t >> 7");
 
-  
+  bytebeat_le();
+}
 
-//  if (button3_state == HIGH)
-//  {
-    fast_bytebeat();
-//  }
+void cv_tv_gfx()
+{
+  TV.select_font(font8x8);
+  TV.println("");
+  TV.println("");
+  TV.println("");
+  TV.println("CV TV");
+  cv_tv();
 }
 
 void display_menu()
@@ -187,6 +273,10 @@ void display_menu()
   if (menu_state == 5) {TV.println("OSCILLOGRAFIX <----");}
   else if (menu_state != 5) {TV.println("OSCILLOGRAFIX");}
 
+  //MENU ITEM 6 - 
+  if (menu_state == 6) {TV.println("DEBUG <------------");}
+  else if (menu_state != 6) {TV.println("DEBUG");}
+
   delay(button_delay);
 }
 
@@ -196,6 +286,8 @@ void control_poller()
   button2_state = digitalRead(button2_pin);
   button3_state = digitalRead(button3_pin);
   button4_state = digitalRead(button4_pin);
+  trig1_state = digitalRead(trig1_pin);
+  trig2_state = digitalRead(trig2_pin);
 
   pot1_val = analogRead(pot1_pin);
   pot2_val = analogRead(pot2_pin);
@@ -220,13 +312,13 @@ void control_interpreter()
     delay(button_delay);
   }
   //BUTTON 1 - ENTER
-  if (button1_state == HIGH && enter_state == LOW)
+  if (button3_state == HIGH && enter_state == LOW)
   {
     enter_state = HIGH;
     delay(button_delay);
   }
   //BUTTON 3 - BACK
-  else if (button3_state == HIGH && enter_state == HIGH)
+  else if (button1_state == HIGH && enter_state == HIGH)
   {
     enter_state = LOW;
     delay(button_delay);
@@ -245,10 +337,6 @@ void splashscreen()
   TV.println("A FUxxeD UP TV");
   TV.println("");
   TV.delay(3000);
-
-  TV.select_font(font6x8);
-  TV.println("        2k18");
-  delay(2000);
  
   TV.clear_screen();
 }
